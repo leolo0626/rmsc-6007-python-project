5f1a0f28b6955c46ce53f19359458f41be388129
leolo <leolo0626@gmail.com>
2023-04-29 01:08:06 +0800

    add Hsi strategy and data

--- src/strategy/HsiStrategy.py
+++ src/strategy/HsiStrategy.py
@@ -37,29 +37,39 @@ def download_and_save_asset_prices():
 
 
 if __name__ == "__main__":
-
     orig_asset_price = pd.read_csv('../data/HSI Index/hsi_con_prices_20210607.csv')
     orig_asset_price.set_index('Date', inplace=True)
     asset_price = orig_asset_price[orig_asset_price.index >= '2021-06-07']
 
-    # # CRP
-    # weights = CRP().run(asset_price)
-    # algo_result = get_algo_result(asset_price, weights)
-    # algo_result.fee = 0.02 / 100
-    # print(algo_result.summary())
-    #
-    # # index price
-    # index_price = pd.read_csv('../data/HSI Index/hsi_index_price.csv')
-    # index_price.set_index('Date', inplace=True)
-    # index_result = get_algo_result(index_price, [1 for i in range(len(index_price))])
-    # index_result.fee = 0
-    # print(index_result.summary())
+    # CRP
+    weights = CRP().run(asset_price)
+    algo_result = get_algo_result(asset_price, weights)
+    algo_result.fee = 0.02 / 100
+    print(algo_result.summary())
+    # {'annualized_return': -0.26274719765320453, 'annualized_volatility': 0.15518998399928477,
+    #  'sharpe_ratio': -1.6930680117501367, 'mdd': 0.11315066601696189, 'calmar_ratio': -2.3221003190013687,
+    #  'final_wealth': 0.9346237485895997}
 
-    # CORN
+    # index price
+    index_price = pd.read_csv('../data/HSI Index/hsi_index_price.csv')
+    index_price.set_index('Date', inplace=True)
+    index_result = get_algo_result(index_price, [1 for i in range(len(index_price))])
+    index_result.fee = 0
+    print(index_result.summary())
+    # {'annualized_return': -0.40591224034260037, 'annualized_volatility': 0.21547679664078853,
+    #  'sharpe_ratio': -1.8837863132858712, 'mdd': 0.1515455665603621, 'calmar_ratio': -2.6784831094410237,
+    #  'final_wealth': 0.8997720822192036}
+
+    # CORN (Time consuming)
+    # Need to run between different parameter
     opt_weight_param = TCAdjustedReturnOptWeightParam(
         fee=0.2 / 100,
         lda=0.5
     )
-    corn_weights = CORN(window_size=30, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(orig_asset_price)
-    print(corn_weights)
-
+    corn_weights = CORN(window_size=10, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(orig_asset_price)
+    algo_result = get_algo_result(asset_price, corn_weights[-len(asset_price):])
+    algo_result.fee = 0.02 / 100
+    print(algo_result.summary)
+    # {'annualized_return': -0.17875497727374343, 'annualized_volatility': 0.15733456280545932,
+    #  'sharpe_ratio': -1.1361456382268018, 'mdd': 0.10299288669357753, 'calmar_ratio': -1.7356050792669966,
+    #  'final_wealth': 0.9540816941031289}
