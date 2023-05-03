3ca21ceec3b10304ea65f174779aa88e57707a50
Andrson <anderson1531222@gmil.com>
2023-05-03 17:16:27 +0800

    Added some graphs

--- src/strategy/MultiAssetStrategy.py
+++ src/strategy/MultiAssetStrategy.py
@@ -4,6 +4,8 @@ from src.algo.CRP import CRP
 from src.algo_result import get_algo_result
 from src.data_provider.yahoo_finance_data_provider import YahooFinanceDataProvider
 import pandas as pd
+import plotly.express as px
+
 
 from src.model.opt_weight_param import TCAdjustedReturnOptWeightParam
 from src.strategy.strategy_helper import get_algo_runner_req_corn, algo_runner_mp_corn
@@ -25,15 +27,7 @@ def download_and_save_asset_prices():
 
 # Todo: Optimize different parameters
 
-def algo_container():
-    ################# Bear ########################
-    # backtest_start = '2022-08-15'
-    # backtest_end = '2022-11-13'
-    # orig_asset_price = pd.read_csv('../data/ETF/us_multi_asset_prices_bear.csv')
-    # orig_asset_price.set_index('Date', inplace=True)
-    # asset_price = orig_asset_price[(orig_asset_price.index >= backtest_start)
-    #                               & (orig_asset_price.index <= backtest_end)]
-    ################# Bulll ########################
+def algo_container_bull():
     backtest_start = '2021-08-16'
     backtest_end = '2021-11-14'
     orig_asset_price = pd.read_csv('../data/ETF/us_multi_asset_prices_bull.csv')
@@ -42,14 +36,10 @@ def algo_container():
                                    & (orig_asset_price.index <= backtest_end)]
     # CRP
     weights = CRP().run(asset_price)
-    algo_result = get_algo_result(asset_price, weights, "CRP")
-    algo_result.fee = 0.2 / 100
-    print(algo_result.summary())
-    ################# Bear ########################
-    # {'annualized_return': -0.1817835792070297, 'annualized_volatility': 0.22054123494425962,
-    # 'sharpe_ratio': -0.8242611829618818, 'mdd': 0.13969165879989487, 'calmar_ratio': -1.3013202131662747,
-    # 'final_wealth': 0.9498798816852565}
-    ################# Bulll ########################
+    crp_result = get_algo_result(asset_price, weights, "CRP")
+    crp_result.fee = 0.2 / 100
+    print(crp_result.summary())
+    ################# Bull ########################
     # {'annualized_return': 0.17394676087795613, 'annualized_volatility': 0.09072216165688786,
     # 'sharpe_ratio': 1.9173568806244339, 'mdd': 0.037861662670064744, 'calmar_ratio': 4.594271582676342,
     # 'final_wealth': 1.043373482723385}
@@ -59,11 +49,7 @@ def algo_container():
     index_result = get_algo_result(index_price, [1 for i in range(len(index_price))], "benchmark")
     index_result.fee = 0
     print(index_result.summary())
-    ################# Bear ########################
-    # {'annualized_return': -0.2406170015909761, 'annualized_volatility': 0.2725629556374567,
-    #  'sharpe_ratio': -0.8827942191492348, 'mdd': 0.1668032180043243, 'calmar_ratio': -1.4425201412165694,
-    #  'final_wealth': 0.9330481574567427}
-    ################# Bulll ########################
+    ################# Bull ########################
     # {'annualized_return': 0.19693035435412165, 'annualized_volatility': 0.11395130149297053,
     # 'sharpe_ratio': 1.728197499931758, 'mdd': 0.0511413805389247, 'calmar_ratio': 3.85070469899095,
     # 'final_wealth': 1.0487660623786357}
@@ -75,17 +61,77 @@ def algo_container():
     )
     corn_weights = CORN(window_size=10, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(orig_asset_price)
 
-    algo_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
-    algo_result.fee = 0.2 / 100
-    print(algo_result.summary())
+    corn_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
+    corn_result.fee = 0.2 / 100
+    print(corn_result.summary())
+    ################# Bull ########################
+    # {'annualized_return': 0.1945308830264043, 'annualized_volatility': 0.10601163044237195,
+    # 'sharpe_ratio': 1.8349956718395302, 'mdd': 0.04718347020899971, 'calmar_ratio': 4.12286086980732,
+    # 'final_wealth': 1.048363796999007}
+
+    equity_curve_df = pd.DataFrame(
+        {crp_result.algo_name: crp_result.equity_curve,
+         index_result.algo_name: index_result.equity_curve,
+         corn_result.algo_name: corn_result.equity_curve},
+    )
+
+    fig = px.line(equity_curve_df, title='Equity Curve for Multi-Asset ETF Strategy (Bull) - 2021/08/16 to 2021/11/14')
+    fig.show()
+
+
+def algo_container_bear():
+    backtest_start = '2022-08-15'
+    backtest_end = '2022-11-13'
+    orig_asset_price = pd.read_csv('../data/ETF/us_multi_asset_prices_bear.csv')
+    orig_asset_price.set_index('Date', inplace=True)
+    asset_price = orig_asset_price[(orig_asset_price.index >= backtest_start)
+                                  & (orig_asset_price.index <= backtest_end)]
+
+    # CRP
+    weights = CRP().run(asset_price)
+    crp_result = get_algo_result(asset_price, weights, "CRP")
+    crp_result.fee = 0.2 / 100
+    print(crp_result.summary())
+    ################# Bear ########################
+    # {'annualized_return': -0.1817835792070297, 'annualized_volatility': 0.22054123494425962,
+    # 'sharpe_ratio': -0.8242611829618818, 'mdd': 0.13969165879989487, 'calmar_ratio': -1.3013202131662747,
+    # 'final_wealth': 0.9498798816852565}
+
+    # Index
+    index_price = pd.DataFrame(asset_price['SPY'])
+    index_result = get_algo_result(index_price, [1 for i in range(len(index_price))], "benchmark")
+    index_result.fee = 0
+    print(index_result.summary())
+    ################# Bear ########################
+    # {'annualized_return': -0.2406170015909761, 'annualized_volatility': 0.2725629556374567,
+    #  'sharpe_ratio': -0.8827942191492348, 'mdd': 0.1668032180043243, 'calmar_ratio': -1.4425201412165694,
+    #  'final_wealth': 0.9330481574567427}
+
+    # CORN
+    opt_weight_param = TCAdjustedReturnOptWeightParam(
+        fee=0.2 / 100,
+        lda=0.5
+    )
+    corn_weights = CORN(window_size=10, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(
+        orig_asset_price)
+
+    corn_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
+    corn_result.fee = 0.2 / 100
+    print(corn_result.summary())
     ################# Bear ########################
     # {'annualized_return': -0.14666075028987846, 'annualized_volatility': 0.19234060800363267,
     # 'sharpe_ratio': -0.762505389850429, 'mdd': 0.12047410592963059, 'calmar_ratio': -1.2173632595832966,
     # 'final_wealth': 0.9596259114353459}
-    ################# Bulll ########################
-    # {'annualized_return': 0.1945308830264043, 'annualized_volatility': 0.10601163044237195,
-    # 'sharpe_ratio': 1.8349956718395302, 'mdd': 0.04718347020899971, 'calmar_ratio': 4.12286086980732,
-    # 'final_wealth': 1.048363796999007}
+
+    equity_curve_df = pd.DataFrame(
+        {crp_result.algo_name: crp_result.equity_curve,
+         index_result.algo_name: index_result.equity_curve,
+         corn_result.algo_name: corn_result.equity_curve},
+    )
+
+    fig = px.line(equity_curve_df, title='Equity Curve for Multi-Asset ETF Strategy (Bear) - 2022/08/15 to 2022/11/13')
+    fig.show()
+
 
     # Todo: Optimize different parameters
 
@@ -98,8 +144,9 @@ if __name__ == "__main__":
     #bull_asset_prices.set_index('Date', inplace=True)
     #reqs = get_algo_runner_req_corn()
     #algo_runner_mp_corn(bull_asset_prices, reqs, 'data/ETF/Bull')
-    bear_asset_prices = pd.read_csv('../data/ETF/us_multi_asset_prices_bear.csv')
-    bear_asset_prices.set_index('Date', inplace=True)
-    reqs = get_algo_runner_req_corn()
-    algo_runner_mp_corn(bear_asset_prices, reqs, 'data/ETF/Bear')
 
+    # bear_asset_prices = pd.read_csv('../data/ETF/us_multi_asset_prices_bear.csv')
+    # bear_asset_prices.set_index('Date', inplace=True)
+    # reqs = get_algo_runner_req_corn()
+    # algo_runner_mp_corn(bear_asset_prices, reqs, 'data/ETF/Bear')
+    algo_container_bear()
--- src/strategy/SEC13FStrategy.py
+++ src/strategy/SEC13FStrategy.py
@@ -4,6 +4,7 @@ from src.algo_result import get_algo_result
 from src.data_provider.fintel_data_provider import FintelDataProvider
 from src.data_provider.yahoo_finance_data_provider import YahooFinanceDataProvider
 import pandas as pd
+import plotly.express as px
 
 # print(assets)
 # 2022-08-15 #2022-11-14
@@ -31,18 +32,12 @@ def download_and_save_asset_prices():
     #asset_price.to_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2021-06-30.csv',
     #                   index=True)
 
-def algo_container():
-    backtest_start = '2022-08-15'
-    backtest_end = '2022-11-13'
-    current_holdings = FintelDataProvider().get_current_holdings('berkshire-hathaway', '2022-06-30')
+def algo_container_bull():
+    backtest_start = '2021-08-16'
+    backtest_end = '2021-11-14'
+    current_holdings = FintelDataProvider().get_current_holdings('berkshire-hathaway', '2021-06-30')
     assets = current_holdings.symbol.tolist()
-    orig_asset_price = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2022-06-30.csv')
-    #################### Bull ################################
-    # backtest_start = '2021-08-16'
-    # backtest_end = '2021-11-14'
-    # current_holdings = FintelDataProvider().get_current_holdings('berkshire-hathaway', '2021-06-30')
-    # assets = current_holdings.symbol.tolist()
-    # orig_asset_price = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2021-06-30.csv')
+    orig_asset_price = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2021-06-30.csv')
     orig_asset_price.set_index('Date', inplace=True)
     asset_price = orig_asset_price[(orig_asset_price.index >= backtest_start)
                                    & (orig_asset_price.index <= backtest_end)]
@@ -53,13 +48,9 @@ def algo_container():
                                                        ch_for_price_only.current_value_1000.sum()
     # CRP
     weights = CRP().run(asset_price)
-    algo_result = get_algo_result(asset_price, weights, "CRP")
-    algo_result.fee = 0.2 / 100
-    print(algo_result.summary())
-    ####################### Bear #################################
-    # {'annualized_return': -0.15067973243057642, 'annualized_volatility': 0.26935071983883085,
-    # 'sharpe_ratio': -0.559418339482209, 'mdd': 0.16403099766230245, 'calmar_ratio': -0.9186052305844482,
-    # 'final_wealth': 0.9544947884623867}
+    crp_result = get_algo_result(asset_price, weights, "CRP")
+    crp_result.fee = 0.2 / 100
+    print(crp_result.summary())
     ####################### Bull #################################
     # {'annualized_return': 0.07268065127170242, 'annualized_volatility': 0.1086472278884723,
     # 'sharpe_ratio': 0.6689600156785409, 'mdd': 0.05185758429667253, 'calmar_ratio': 1.401543327894007,
@@ -69,13 +60,9 @@ def algo_container():
     benchmark_weight = [ch_for_price_only[ch_for_price_only['symbol'] == a].iloc[0].portfolio_weight_price_only
                         for a in asset_price.columns]
     benchmark_weight_t = [benchmark_weight] * len(asset_price)
-    algo_result = get_algo_result(asset_price, benchmark_weight_t, "benchmark")
-    algo_result.fee = 0.2 / 100
-    print(algo_result.summary())
-    ####################### Bear #################################
-    # {'annualized_return': -0.16604138845046412, 'annualized_volatility': 0.29653590679385355,
-    # 'sharpe_ratio': -0.5599368732296326, 'mdd': 0.17059259293376228, 'calmar_ratio': -0.9733212069467443,
-    # 'final_wealth': 0.9490600435353679}
+    index_result = get_algo_result(asset_price, benchmark_weight_t, "benchmark")
+    index_result.fee = 0.2 / 100
+    print(index_result.summary())
     ####################### Bull #################################
     # {'annualized_return': 0.11965429859354293, 'annualized_volatility': 0.13489322867045372,
     # 'sharpe_ratio': 0.8870296883905142, 'mdd': 0.05301199955822056, 'calmar_ratio': 2.2571172487491684,
@@ -88,24 +75,89 @@ def algo_container():
     )
     corn_weights = CORN(window_size=10, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(orig_asset_price)
 
-    algo_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
-    algo_result.fee = 0.2 / 100
-    print(algo_result.summary())
-    ####################### Bear #################################
-    # {'annualized_return': -0.15067973243057642, 'annualized_volatility': 0.26935071983883085,
-    # 'sharpe_ratio': -0.559418339482209, 'mdd': 0.16403099766230245, 'calmar_ratio': -0.9186052305844482,
-    # 'final_wealth': 0.9544947884623867}
+    corn_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
+    corn_result.fee = 0.2 / 100
+    print(corn_result.summary())
     ####################### Bull #################################
     # {'annualized_return': 0.07268065127170242, 'annualized_volatility': 0.1086472278884723,
     # 'sharpe_ratio': 0.6689600156785409, 'mdd': 0.05185758429667253, 'calmar_ratio': 1.401543327894007,
     # 'final_wealth': 1.0168550054417258}
 
+    equity_curve_df = pd.DataFrame(
+        {crp_result.algo_name: crp_result.equity_curve,
+         index_result.algo_name: index_result.equity_curve,
+         corn_result.algo_name: corn_result.equity_curve},
+    )
+
+    fig = px.line(equity_curve_df, title='Equity Curve for 13-F Strategy (Bull) - 2021/08/16 to 2021/11/14')
+    fig.show()
+
+def algo_container_bear():
+    backtest_start = '2022-08-15'
+    backtest_end = '2022-11-13'
+    current_holdings = FintelDataProvider().get_current_holdings('berkshire-hathaway', '2022-06-30')
+    assets = current_holdings.symbol.tolist()
+    orig_asset_price = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2022-06-30.csv')
+    orig_asset_price.set_index('Date', inplace=True)
+    asset_price = orig_asset_price[(orig_asset_price.index >= backtest_start)
+                                   & (orig_asset_price.index <= backtest_end)]
+
+    # Current holding for price only
+    ch_for_price_only = current_holdings[current_holdings.symbol.isin(asset_price.columns)]
+    ch_for_price_only['portfolio_weight_price_only'] = ch_for_price_only.current_value_1000 / \
+                                                       ch_for_price_only.current_value_1000.sum()
+    # CRP
+    weights = CRP().run(asset_price)
+    crp_result = get_algo_result(asset_price, weights, "CRP")
+    crp_result.fee = 0.2 / 100
+    print(crp_result.summary())
+    ####################### Bear #################################
+    # {'annualized_return': -0.15067973243057642, 'annualized_volatility': 0.26935071983883085,
+    # 'sharpe_ratio': -0.559418339482209, 'mdd': 0.16403099766230245, 'calmar_ratio': -0.9186052305844482,
+    # 'final_wealth': 0.9544947884623867}
+
+    # benchmark
+    benchmark_weight = [ch_for_price_only[ch_for_price_only['symbol'] == a].iloc[0].portfolio_weight_price_only
+                        for a in asset_price.columns]
+    benchmark_weight_t = [benchmark_weight] * len(asset_price)
+    index_result = get_algo_result(asset_price, benchmark_weight_t, "benchmark")
+    index_result.fee = 0.2 / 100
+    print(index_result.summary())
+    ####################### Bear #################################
+    # {'annualized_return': -0.16604138845046412, 'annualized_volatility': 0.29653590679385355,
+    # 'sharpe_ratio': -0.5599368732296326, 'mdd': 0.17059259293376228, 'calmar_ratio': -0.9733212069467443,
+    # 'final_wealth': 0.9490600435353679}
+
+    # CORN
+    opt_weight_param = TCAdjustedReturnOptWeightParam(
+        fee=0.2 / 100,
+        lda=0.5
+    )
+    corn_weights = CORN(window_size=10, corr_threshold=0.3, opt_weights_param=opt_weight_param).run(orig_asset_price)
+
+    corn_result = get_algo_result(asset_price, corn_weights[-len(asset_price):], "CORN")
+    corn_result.fee = 0.2 / 100
+    print(corn_result.summary())
+    ####################### Bear #################################
+    # {'annualized_return': -0.15067973243057642, 'annualized_volatility': 0.26935071983883085,
+    # 'sharpe_ratio': -0.559418339482209, 'mdd': 0.16403099766230245, 'calmar_ratio': -0.9186052305844482,
+    # 'final_wealth': 0.9544947884623867}
+
+    equity_curve_df = pd.DataFrame(
+        {crp_result.algo_name: crp_result.equity_curve,
+         index_result.algo_name: index_result.equity_curve,
+         corn_result.algo_name: corn_result.equity_curve},
+    )
+
+    fig = px.line(equity_curve_df, title='Equity Curve for 13-F Strategy (Bear) - 2022/08/15 to 2022/11/13')
+    fig.show()
 
 if __name__ == "__main__":
-    bull_asset_prices = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2021-06-30.csv')
-    bull_asset_prices.set_index('Date', inplace=True)
-    reqs = get_algo_runner_req_corn()
-    algo_runner_mp_corn(bull_asset_prices, reqs, 'data/13-F/bull')
+    # bull_asset_prices = pd.read_csv('../data/13-F/berkshire-hathaway/berkshire-hathaway_asset_price_2021-06-30.csv')
+    # bull_asset_prices.set_index('Date', inplace=True)
+    # reqs = get_algo_runner_req_corn()
+    # algo_runner_mp_corn(bull_asset_prices, reqs, 'data/13-F/bull')
+    algo_container_bull()
 
 
     # # Todo: Optimize different parameters
